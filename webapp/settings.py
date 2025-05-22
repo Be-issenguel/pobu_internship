629ea7f0f15fd99000fe7f4753d7b4ae69012985
Bernardo Issenguel <beissenguel12@gmail.com>
2025-05-22 11:17:56 +0100

    uninstall django-environ and install dotev package

--- webapp/settings.py
+++ webapp/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
-import environ
+import dotenv
 import dj_database_url
 from pathlib import Path
 
@@ -19,17 +19,17 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Take environment variables from .env file
-env = environ.Env()
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+dotenv.load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY', default='#thisisarandomstringandshouldbereplacedinenv')
+SECRET_KEY = os.environ.get('SECRET_KEY', '#thisisarandomstringandshouldbereplacedinenv')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG', cast=bool, default=False)
+
+DEBUG = os.environ.get('DEBUG', True)
 
 ALLOWED_HOSTS = []
 INTERNAL_IPS = ['127.0.0.1', 'localhost']
